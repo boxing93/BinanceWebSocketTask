70ddf5e0689d15a6a7ba3492cd7487f3bc026d75
iliya <iliyaValchev93@gmail.com>
2024-06-05 01:11:21 +0300

    add  not found exception

---
+++ BinanceWebSocketTask.Application/Common/Enums/TimePeriod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace BinanceWebSocketTask.Application.Common.Enums
+{
+    /// <summary>
+    /// Time period in minutes
+    /// </summary>
+    public enum TimePeriod
+	{
+        Week = 10080,
+        Day = 1440,
+        HalfHour = 30,
+        FiveMin = 5,
+        OneMin = 1
+	}
+}
+
